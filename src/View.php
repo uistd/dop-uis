be64e725a089d2782ba8b70ee64564f64234296d
huangshunzhao <huangshunzhao@wanda.cn>
2017-10-30 14:03:42 +0800

    临时方案，只要数据里带 data 就会和 外面的data合并

--- src/View.php
+++ src/View.php
@@ -175,8 +175,10 @@ class View
         if (Response::STATUS_OK === $status && $this->response) {
             $result_data = $this->response->getOutput();
             //如果 数组只有一层, 并且key 也是 data
-            if (is_array($result_data) && isset($result_data['data']) && 1 === count($result_data)) {
-                $result_data = $result_data['data'];
+            if (is_array($result_data) && isset($result_data['data'])) {
+                $result += $result_data;
+            } else {
+                $result['data'] = $result_data;
             }
         }
         //附加数据输出， 但是不会覆盖主体 status message data 字段
@@ -184,7 +186,6 @@ class View
         if (0 === $status && !empty($append_data)) {
             $result += $append_data;
         }
-        $result['data'] = $result_data;
         return $result;
     }
 }
