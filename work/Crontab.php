7b1aa433b2f44e2c44be9bfeb1b2e59a4d45505a
huangshunzhao <huangshunzhao@wanda.cn>
2017-11-12 23:52:32 +0800

    work update

--- work/Crontab.php
+++ work/Crontab.php
@@ -2,6 +2,7 @@
 
 namespace FFan\Uis\Work;
 
+use FFan\Std\Console\Debug;
 use FFan\Std\Logger\FileLogger;
 use FFan\Std\Logger\LogHelper;
 use FFan\Std\Logger\LogRouter;
@@ -13,6 +14,11 @@ use FFan\Std\Logger\LogRouter;
 class Crontab
 {
     /**
+     * @var int 休眠时间
+     */
+    private $sleep_time;
+
+    /**
      * @var LogRouter
      */
     private $logger;
@@ -21,6 +27,16 @@ class Crontab
      * @var string 日志目录
      */
     protected static $log_path = 'crontab';
+
+    /**
+     * @var bool 是否循环执行
+     */
+    protected $is_loop = false;
+
+    /**
+     * @var int 如果是循环执行，每次执行的间隔
+     */
+    protected $loop_sleep = 1000;
 
     /**
      * Crontab constructor.
@@ -34,6 +50,15 @@ class Crontab
             return;
         }
         $this->logMsg('start');
+        if ($this->is_loop) {
+            $this->loop();
+        } else {
+            try {
+                $this->action();
+            } catch (\Exception $exception) {
+                Debug::recordException($exception);
+            }
+        }
     }
 
     /**
@@ -65,9 +90,32 @@ class Crontab
     /**
      * 运行
      */
-    public function action()
+    protected function action()
     {
 
+    }
+
+    /**
+     * 主循环
+     */
+    private function loop()
+    {
+        while ($this->is_loop) {
+            $start_time = microtime(true);
+            try {
+                $this->action();
+            } catch (\Exception $exception) {
+                Debug::recordException($exception);
+            }
+            $now_time = microtime(true);
+            $cost_time = $now_time - $start_time;
+            //转成 微秒
+            $cost_time *= 1000;
+            $sleep_time = $this->sleep_time - $cost_time;
+            if ($sleep_time > 0) {
+                usleep($sleep_time);
+            }
+        }
     }
 
     /**
